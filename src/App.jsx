18f13c08b83f49aa6ffe17236d7450ae28d1be41
Nik <Tilija_nik@hotmail.co.uk>
2024-04-26 14:14:51 +0100

    trying to input arrays and making changes in buyupgrade

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,52 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import upgrades from "./stores/upgrades";
 
 export default function App() {
   const [coins, setCoins] = useState(0);
   const [cps, setCps] = useState(1);
 
+  useEffect(() => {
+    const intervalCoins = setInterval(() => {
+      setCoins((coins) => coins + cps);
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalCoins);
+    };
+  }, [cps]);
+
   function addCoins() {
+    console.log("Adding 1 coin");
     setCoins((coins) => coins + 1);
   }
 
-  function buyUpgrade() {
-    setCps((cps) => cps + 1);
+  function buyUpgrade(upgrade) {
+    if (coins >= upgrade.cost) {
+      console.log("buy an upgrade", upgrade);
+      setCoins((coins) => coins - upgrade.cost);
+      setCps((cps) => cps + upgrade.increment);
+    } else {
+      console.log("not enough coins to upgrade!");
+    }
   }
+
+  console.log("current coins", coins);
+  console.log("current CPS", cps);
 
   return (
     <div>
       <h1>Gallywix's Bank heist</h1>
-      <button onClick={addCoins}>Get a Coin</button>
-      <button onClick={buyUpgrade}>I need Moar Coins</button>
+      <button onClick={addCoins}>Steal 1 Gold Coin</button>
       <p>You have stolen this many {coins} coins</p>
-      <p>You get this many {coins} coins per second </p>
+      <p>You're earning {cps} coins per second </p>
+      <h2>Upgrade for faster coin gains:</h2>
+      {upgrades.map((upgrade, index) => (
+        <div key={index}>
+          <button onClick={() => buyUpgrade(upgrade)}>
+            Cost: {upgrade.cost} Toget: {upgrade.increment}x faster cookies
+          </button>
+        </div>
+      ))}
     </div>
   );
 }
---
+++ src/stores/upgrades.js
@@ -0,0 +1,20 @@
+export const upgrades = [
+  {
+    cost: 10,
+    increment: 1,
+  },
+  {
+    cost: 50,
+    increment: 5,
+  },
+  {
+    cost: 100,
+    increment: 10,
+  },
+  {
+    cost: 1000,
+    increment: 100,
+  },
+];
+
+export default upgrades;
