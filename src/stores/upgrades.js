6a825ba14a16dd30db85b4ad3c45fa4b0466c8ab
Nik <Tilija_nik@hotmail.co.uk>
2024-04-28 16:46:00 +0100

    added more scores

--- src/stores/upgrades.js
+++ src/stores/upgrades.js
@@ -4,16 +4,20 @@ export const upgrades = [
     increment: 1,
   },
   {
-    cost: 50,
+    cost: 100,
     increment: 5,
   },
   {
-    cost: 100,
+    cost: 1000,
     increment: 10,
   },
   {
-    cost: 1000,
+    cost: 10000,
     increment: 100,
+  },
+  {
+    cost: 20000,
+    increment: 200,
   },
 ];
 
