bb5e41549133cd476fb98a96a0a37ef9b88f91e0
Nik <Tilija_nik@hotmail.co.uk>
2024-09-22 11:48:22 +0100

    changes

--- vite.config.js
+++ vite.config.js
@@ -10,10 +10,10 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  root: ".", // Use the root directory
+  root: ".", // Set root to the project directory
   build: {
     rollupOptions: {
-      input: "./index.html", // Tell Vite that index.html is in the root
+      input: "./index.html", // Ensure the path is correct to index.html
     },
   },
 });
