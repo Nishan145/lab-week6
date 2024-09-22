3009381957cc23a02aa12b3f17d8948b2a5401d8
Nik <Tilija_nik@hotmail.co.uk>
2024-09-22 11:35:56 +0100

    running

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+import { defineConfig } from "vite";
+
 export default defineConfig({
-  plugins: [react()],
-})
+  root: "./", // Ensure this points to the correct directory
+  build: {
+    rollupOptions: {
+      input: "./index.html", // Ensure Vite knows where to find the entry
+    },
+  },
+});
